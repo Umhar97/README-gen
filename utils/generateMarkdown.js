af23296651d47edb5abbe70e777d9ecc6675ba34
Umhar Hussain <130864554+Umhar97@users.noreply.github.com>
2023-10-10 21:33:55 +0100

    added function that renders license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,11 @@
 // function to generate markdown for README
+function renderLicenseBadge(license) {
+  if(license!== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+  return ''
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
